22196571f98907fd118f7218910eb26669c6e1e5
Nishan <nishan.bhattrai@aitcentre.com>
2025-03-25 14:34:50 +0545

    small changes

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import { CustomZodValidationPipe } from './global/pipes/zod.validation.pipe';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: '*', // Change this to your frontend URL if needed
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
   app.useGlobalPipes(new CustomZodValidationPipe());
   app.useGlobalFilters(new GlobalExceptionFilter());
   app.setGlobalPrefix(process.env.BASE_URL ?? 'api/v1');
