d81bc7fe8928f6b1a85103d637897bf0a40cefdd
Nishan <nishan.bhattrai@aitcentre.com>
2025-03-27 14:21:44 +0545

    feat: home services created

---
+++ src/utils/homedata.ts
@@ -0,0 +1,140 @@
+export const homedate = {
+  location: {
+    city: 'New York',
+    country: 'USA',
+  },
+  upcomingSchedule: [
+    {
+      date: '2021-10-10',
+      starttime: '10:00',
+      endtime: '12:00',
+      phoneNumber: '1234567890',
+      doctor: {
+        name: 'Dr. John Doe',
+        specialty: 'Cardiology',
+      },
+    },
+    {
+      date: '2021-10-10',
+      starttime: '10:00',
+      endtime: '12:00',
+      phoneNumber: '1234567890',
+      doctor: {
+        name: 'Dr. Alana Rueter',
+        specialty: 'Dentist',
+      },
+    },
+    {
+      date: '2021-10-10',
+      starttime: '10:00',
+      endtime: '12:00',
+      phoneNumber: '1234567890',
+      doctor: {
+        name: 'Dr. Alana Rueter',
+        specialty: 'Dentist',
+      },
+    },
+  ],
+  speciality: [
+    {
+      icon: 'https://cdn-icons-png.flaticon.com/512/2924/2924821.png',
+      name: 'Cardiology',
+    },
+    {
+      icon: 'https://cdn-icons-png.flaticon.com/512/2924/2924821.png',
+      name: 'Dentist',
+    },
+    {
+      icon: 'https://cdn-icons-png.flaticon.com/512/2924/2924821.png',
+      name: 'Cardiology',
+    },
+    {
+      icon: 'https://cdn-icons-png.flaticon.com/512/2924/2924821.png',
+      name: 'Neurology',
+    },
+    {
+      icon: 'https://cdn-icons-png.flaticon.com/512/2924/2924821.png',
+      name: 'Orthology',
+    },
+    {
+      icon: 'https://cdn-icons-png.flaticon.com/512/2924/2924821.png',
+      name: 'Cardiology',
+    },
+    {
+      icon: 'https://cdn-icons-png.flaticon.com/512/2924/2924821.png',
+      name: 'Orthology',
+    },
+    {
+      icon: 'https://cdn-icons-png.flaticon.com/512/2924/2924821.png',
+      name: 'Cardiology',
+    },
+  ],
+  nearbyHospitals: [
+    {
+      name: 'New York Hospital',
+      image: 'https://cdn-icons-png.flaticon.com/512/2924/2924821.png',
+      distance: '2.5 km',
+      time: 15,
+      rating: 4.5,
+      whishist: true,
+    },
+    {
+      name: 'New York Hospital',
+      image: 'https://cdn-icons-png.flaticon.com/512/2924/2924821.png',
+      distance: '2.5 km',
+      time: 10,
+      rating: 4.5,
+      whishist: false,
+    },
+    {
+      name: 'New York Hospital',
+      image: 'https://cdn-icons-png.flaticon.com/512/2924/2924821.png',
+      distance: '2.5 km',
+      time: 15,
+      rating: 4.5,
+      whishist: true,
+    },
+    {
+      name: 'New York Hospital',
+      image: 'https://cdn-icons-png.flaticon.com/512/2924/2924821.png',
+      distance: '2.5 km',
+      time: 10,
+      rating: 4.5,
+      whishist: false,
+    },
+  ],
+  specialist: [
+    {
+      icon: 'https://cdn-icons-png.flaticon.com/512/2924/2924821.png',
+      name: 'Dr. John Doe',
+      specialty: 'Cardiology',
+      rating: 4.5,
+      review: 68,
+      wishlist: true,
+    },
+    {
+      icon: 'https://cdn-icons-png.flaticon.com/512/2924/2924821.png',
+      name: 'Dr. John Doe',
+      specialty: 'Cardiology',
+      rating: 4.5,
+      review: 22,
+      wishlist: false,
+    },
+    {
+      icon: 'https://cdn-icons-png.flaticon.com/512/2924/2924821.png',
+      name: 'Dr. Jane Cooper',
+      specialty: 'Cardiology',
+      rating: 4.5,
+      review: 83,
+      wishlist: false,
+    },
+    {
+      icon: 'https://cdn-icons-png.flaticon.com/512/2924/2924821.png',
+      name: 'Dr. Robert Fox',
+      specialty: 'Cardiology',
+      rating: 4.5,
+      review: 48,
+      wishlist: true,
+    },
+  ],
+};
